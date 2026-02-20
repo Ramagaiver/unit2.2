2703afa85950ab74684614cc54b37d2f9c7482b3
diplomattim <diplomatguillotine@gmail.com>
2026-02-20 12:49:42 -0600

    Implemented iterative layer adding and StateOutline & Natives1870

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ function createMap(){
     // Creates the map
     map = L.map('map', {
         center: [39.328, -111.674],
-        zoom: 7
+        zoom: 7,
+        minZoom: 7,
+        maxBounds: ([
+            [44.001, -116.043],
+            [35.001, -107.047],
+        ])
     })
 
     // Initializes the tile layer
@@ -16,12 +21,12 @@ function createMap(){
         // Add the proper ESRI/ARCGIS attribution here //
     }).addTo(map);
     
-    // Runs the getData function; responsible for fetching data, 
-    getData()
+    // Runs the loadData function; responsible for fetching data, 
+    loadData()
 }
 
 // Function that handles getting feature attributes for popups; if there are none then it adds them via an html string
-function onEachFeature(feature, layer) {
+function onEachFeature(feature, layer){
     var popupContent = "";
     if (feature.properties) {
         for (var property in feature.properties){
@@ -32,28 +37,41 @@ function onEachFeature(feature, layer) {
 };
 
 // Function that handles fetching and loading all necessary data
-function getData(){
-    fetch('data/MormonSettlements.geojson')
-        .then(function(response){
-            return response.json();
-        })
-		.then(function(json){
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
+function loadData(){
 
-            L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                },
-                onEachFeature: onEachFeature
-            }).addTo(map);
-        }
-)}
+    // Variable that holds directory of all datasets
+    const geojsonData = [
+    'data/Natives1870.geojson',
+    'data/StateOutlines.geojson',
+    'data/MormonSettlements.geojson'
+    ];  
+
+    for (path of geojsonData){
+        console.log("Fetching " + path)
+        fetch(path)
+            .then(function(response){
+                return response.json();
+            })
+            .then(console.log("Fetched " + path))
+
+            .then(function(json){
+                var geojsonMarkerOptions = {
+                    radius: 8,
+                    fillColor: "#ff7800",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                };
+
+                    L.geoJson(json, {
+                        pointToLayer: function(feature, latlng){
+                            return L.circleMarker(latlng, geojsonMarkerOptions);
+                        },
+                        onEachFeature: onEachFeature
+                    }).addTo(map)} )
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded',createMap)
